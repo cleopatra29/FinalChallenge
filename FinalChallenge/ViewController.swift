b7fa256306f105378414368153fa23c9ae21880c
Jansen Malvin <mwczanez15@gmail.com>
2019-01-18 14:53:12 +0700

    hai terre

--- FinalChallenge/ViewController.swift
+++ FinalChallenge/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("asem")
+        //memel
+        print("terrebajingan")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
